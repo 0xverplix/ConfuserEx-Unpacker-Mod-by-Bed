5f618aa8c14b66ede413b5dd536e5e7119027ec6
BedTheGod <buettnerlogan2@gmail.com>
2019-02-10 22:30:05 -0500

    Changed Compressor (Details in Desc)
    changed the compressor to work with Trinity's new packer. Will put the regular confuserex compressor remover back in soon, both will work together.

--- ConfuserEx Unpacker/ConfuserEx Unpacker/Protections/Compressor/Remover.cs
+++ ConfuserEx Unpacker/ConfuserEx Unpacker/Protections/Compressor/Remover.cs
@@ -7,6 +7,7 @@ using System.IO;
 using System.Linq;
 
 using System.Runtime.InteropServices;
+using System.Security.Cryptography;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -22,7 +23,7 @@ namespace ConfuserEx_Unpacker.Protections.Compressor
         public override void Deobfuscate()
         {
            
-                StepOne(ModuleDef);
+                UnpackModule(ModuleDef);
             
         }
         public static bool IsPacked(ModuleDefMD module)
@@ -42,43 +43,128 @@ namespace ConfuserEx_Unpacker.Protections.Compressor
 
             return false;
         }
-        private void StepOne(ModuleDefMD module)
+        public static string IVkey(MethodDef EntryPoint)
         {
-            var ep = module.EntryPoint;
-          
-                foreach (TypeDef typeDef in module.GetTypes())
+            for (int i = 0; i < EntryPoint.Body.Instructions.Count; i++)
+            {
+                if (EntryPoint.Body.Instructions[i].Operand.ToString().Contains("Convert::FromBase64String") & EntryPoint.Body.Instructions[i-1].Operand.ToString().Length == 24)
                 {
-                    foreach (MethodDef methods in typeDef.Methods)
+                    return EntryPoint.Body.Instructions[i - 1].Operand.ToString();
+                }
+            }
+            return null;
+                }
+        private void UnpackModule(ModuleDefMD module)
+        {
+            bool can = false;
+            
+                if (module.EntryPoint.Body.Instructions[44].Operand.ToString().Contains("Assembly::Load"))
+                {
+                    can = true;
+                    
+                }
+                
+            
+            if(can)
+            try
+            {
+                
+
+                string ivKey = "";
+                MethodDef epp = module.EntryPoint;
+                for (int i = 0; i < module.EntryPoint.Body.Instructions.Count; i++)
+                {
+                    if (module.EntryPoint.Body.Instructions[i].OpCode == OpCodes.Ldstr)
                     {
-                        if (!methods.HasBody) continue;
-                        for (int i = 0; i < methods.Body.Instructions.Count; i++)
+                        if (module.EntryPoint.Body.Instructions[i].Operand.ToString().Length == 24)
                         {
-                            if (methods.Body.Instructions[i].OpCode == OpCodes.Call)
-                            {
-                           
-                                
-                                    if (methods.Body.Instructions[i].Operand.ToString().Contains("Assembly::Load"))
-                                    {
-                              
-                                byte[] bytees = (byte[])methods.Body.Instructions[i-2].Operand;
-                               Protections.Base.ModuleDef = ModuleDefMD.Load(bytees);
-                                  //Protections.Base.ModuleDef.EntryPoint = Protections.Base.ModuleDef.ResolveToken(ModuleEp) as MethodDef;
-                                Base.CompressorRemoved = true;
-
-
-                            }
-
-                        }
+                            ivKey = module.EntryPoint.Body.Instructions[i].Operand.ToString();
                         }
                     }
                 }
+                string key = "";
+                for (int i = 0; i < module.EntryPoint.Body.Instructions.Count; i++)
+                {
+                    if (module.EntryPoint.Body.Instructions[i].OpCode == OpCodes.Ldstr)
+                    {
+                        if (module.EntryPoint.Body.Instructions[i].Operand.ToString().Length == 44)
+                        {
+                            key = module.EntryPoint.Body.Instructions[i].Operand.ToString();
+                        }
+                    }
+                }
+                string arrayKey = "";
+                for (int i = 0; i < module.EntryPoint.Body.Instructions.Count; i++)
+                {
+                    if (module.EntryPoint.Body.Instructions[i].OpCode == OpCodes.Ldstr)
+                    {
+                        if (module.EntryPoint.Body.Instructions[i].Operand.ToString().Length > 200)
+                        {
+                            arrayKey = module.EntryPoint.Body.Instructions[i].Operand.ToString();
+                        }
+                    }
+                }
+
+
+                //string Key = Otherkey(epp);
+                //string bigarray = GetArray(epp);
+                RijndaelManaged rijndaelManaged = new RijndaelManaged();
+                rijndaelManaged.KeySize = 256;
+                rijndaelManaged.Key = Convert.FromBase64String(key);
+                rijndaelManaged.IV = Convert.FromBase64String(ivKey);
+                rijndaelManaged.Padding = PaddingMode.ISO10126;
+                MemoryStream memoryStream = new MemoryStream();
+                CryptoStream cryptoStream = new CryptoStream(memoryStream, rijndaelManaged.CreateDecryptor(), CryptoStreamMode.Write);
+                byte[] array = Convert.FromBase64String(arrayKey);
+                cryptoStream.Write(array, 0, array.Length);
+                cryptoStream.Flush();
+                memoryStream.Seek(0L, SeekOrigin.Begin);
+                Protections.Base.ModuleDef = ModuleDefMD.Load(memoryStream.ToArray());
+                cryptoStream.Close();
+                memoryStream.Close();
+                cryptoStream.Dispose();
+                memoryStream.Dispose();
+
+                //Protections.Base.ModuleDef.EntryPoint = Protections.Base.ModuleDef.ResolveToken(ModuleEp) as MethodDef;
+                Base.CompressorRemoved = true;
+
+
+            }
+            catch(Exception ex)
+            {
+                Console.Write(ex.ToString());
+                    Console.Read();
+            }
+                
             
+        }
+       
         //Protections.Base.ModuleDef = ModuleDefMD.Load(ModuleBytes);
         //    Protections.Base.ModuleDef.EntryPoint = Protections.Base.ModuleDef.ResolveToken(ModuleEp) as MethodDef;
         //    Base.CompressorRemoved = true;
+
+        public static string Otherkey(MethodDef EntryPoint)
+        {
+            for (int i = 0; i < EntryPoint.Body.Instructions.Count; i++)
+            {
+                if (EntryPoint.Body.Instructions[i].Operand.ToString().Contains("Convert::FromBase64String") && EntryPoint.Body.Instructions[i - 1].Operand.ToString().Length == 44)
+                {
+                    return EntryPoint.Body.Instructions[i - 1].Operand.ToString();
+                }
+            }
+            return null;
         }
-
-
+        public static string GetArray(MethodDef EntryPoint)
+        {
+            for (int i = 0; i < EntryPoint.Body.Instructions.Count; i++)
+            {
+                if (EntryPoint.Body.Instructions[i].Operand.ToString().Contains("Convert::FromBase64String") && EntryPoint.Body.Instructions[i - 1].Operand.ToString().Length >100)
+                {
+                    return EntryPoint.Body.Instructions[i - 1].Operand.ToString();
+                }
+            }
+            return null;
+        }
 
 
         public static GCHandle HandleDecryptMethod(Emulation mainEmulation, CallEventArgs mainArgs, MethodDef decryptionMethod)
